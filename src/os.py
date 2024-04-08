586ef79766859ec70eca3577b243fcdbe183fdf5
Usermode <scottjsh@whitesky.kr>
2024-04-08 11:50:19 +0900

    Detect Windows 11

--- src/os.py
+++ src/os.py
@@ -1,11 +1,16 @@
 import platform
+import sys
 
 # This function detects the OS the user is running and if Valorant is officially supported in said platform
 # returns ["operating system" (string), "Runs Valorant" (bool)]
 def get_os():   
     # Handles Windows operating systems
-    if platform.system() == "Windows":
+    if platform.system() == "Windows" and platform.win32_ver()[0] == '10':
             return f"Windows {platform.win32_ver()[0]} {platform.win32_edition()} {platform.win32_ver()[1]}"
+        
+    elif sys.getwindowsversion().build >= 22000:
+        return f"Windows 11 {platform.win32_edition()} {platform.win32_ver()[1]}"
+    
     # Handles other operating systems, such as Linux or Mac OS
     else:
         return "Non-Windows operating system"
